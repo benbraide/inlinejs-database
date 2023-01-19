b50c30a2fad6d5949fb690723973c654a6614919
benbraide <benbraide@yahoo.com>
2023-01-19 09:18:33 +0100

    Enabled entry point; Feature update

---
+++ src/entry.ts
@@ -0,0 +1,16 @@
+import { GetGlobal, WaitForGlobal } from '@benbraide/inlinejs';
+
+import { DatabaseConcept } from './concept';
+
+import { DatabaseMagicHandlerCompact } from './magic/database';
+
+export function InlineJSDatabase(){
+    WaitForGlobal().then(() => {
+        const concept = new DatabaseConcept();
+        
+        GetGlobal().SetConcept('database', concept);
+        GetGlobal().SetConcept('db', concept);
+    
+        DatabaseMagicHandlerCompact();
+    });
+}
--- src/inlinejs-database.ts
+++ src/inlinejs-database.ts
@@ -1,12 +1,3 @@
-import { GetGlobal, WaitForGlobal } from '@benbraide/inlinejs';
+import { InlineJSDatabase } from './entry';
 
-import { DatabaseConcept } from './concept';
-
-import { DatabaseMagicHandlerCompact } from './magic/database';
-
-WaitForGlobal().then(() => {
-    GetGlobal().SetConcept('database', new DatabaseConcept());
-    GetGlobal().SetConcept('db', new DatabaseConcept());
-
-    DatabaseMagicHandlerCompact();
-});
+InlineJSDatabase();
